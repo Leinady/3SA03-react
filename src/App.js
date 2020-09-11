fc6636f432ec8835f72baa3a19486c48e8d7dfaf
Leinady <danielpathan137@gmail.com>
2020-09-11 10:45:44 +0700

    check2/2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
-
+import WordCard from './WordCard';
+const word = "Hello";
 
 function App() {
   return (
     <div>
-
-      <WordCard value="hello"/>
-     
+     <WordCard value="hello"/>
     </div>
   );
 }
