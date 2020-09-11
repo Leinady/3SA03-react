d55419bf7a3ab5037d379f4ae283668c59addf00
Leinady <danielpathan137@gmail.com>
2020-09-11 12:09:34 +0700

    Check 4

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import CharacterCard from './CharacterCard';
+import React , {useState,useEffect}from 'react';
 import _ from 'lodash';
+import CharacterCard from './CharacterCard';
 
-export default function WordCard(props){
-
-    const activationHandler = c => { console.log(`${c} has been activated.`) } 
-
-    const prepareStateFromWord = (given_word) => {
+const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
     return {
@@ -17,25 +13,44 @@ export default function WordCard(props){
         completed: false
     }
 }
-const activationHandler = (c) => {
-    console.log(`${c} has been activated.`)
-    let guess = state.guess + c
-    setState({...state, guess})
-    if(guess.length == state.word.length){
-        if(guess == state.word){
-        console.log('yeah!')
-        setState({...state, guess: '', completed: true})
-    }else{
-        console.log('reset')
-        setState({...state, guess: '', attempt: state.attempt + 1})
+export default function WordCard(props) {
+    const [state, setState] = useState({
+        word: '',
+        chars: '',
+        attempt: 1,
+        guess: '',
+        completed: false
+    })
+    useEffect(() => {
+        let data = prepareStateFromWord(props.value);
+        setState({
+            ...state,
+            word: data.word,
+            chars: data.chars,
+            attempt: data.attempt,
+            guess: data.guess,
+            completed: data.completed
+        })
+    }, [])
+    const activationHandler = (c) => {
+        console.log( `${c} has been activated.`)
+        let guess = state.guess + c
+        setState({ ...state, guess })
+        if (guess.length == state.word.length) {
+            if (guess == state.word) {
+                console.log('yeah!')
+                setState({ ...state, guess: '', completed: true })
+            } else {
+                console.log('reset')
+                setState({ ...state, guess: '', attempt: state.attempt + 1 })
+            }
+        }
     }
-  }
-}
- return (
- <div>
-    { Array.from(state.char).map((c, i) => <CharacterCard value={c} key={i} 
-    activationHandler={activationHandler}
-    attempt = {state.attempt} /> )}
- </div>
- );
+    return (
+        <div>
+            {Array.from(state.chars).map((c, i) => <CharacterCard value={c} key={i} 
+            activationHandler={activationHandler} 
+            attempt={state.attempt}/>)} 
+        </div>
+    );
 }
